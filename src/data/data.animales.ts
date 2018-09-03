96cadd58882b43599c2bf1f68fa80247a60c22fd
Zedrick Rodriguez <inedc4zedrickr@gmail.com>
2018-09-03 17:54:16 -0600

    personajes de peliculas

---
+++ src/data/data.animales.ts
@@ -0,0 +1,73 @@
+
+export const ANIMALES = [
+      {
+        nombre: "Iron Man",
+        imagen: "assets/animales/iron.png",
+        audio: "assets/sonidos/iron.mp3",
+        duracion: 4,
+        reproduciendo: false
+      },
+      {
+        nombre: "Deadpool",
+        imagen: "assets/animales/deadpool.png",
+        audio: "assets/sonidos/deadpool.mp3",
+        duracion: 4,
+        reproduciendo: false
+      },
+      {
+        nombre: "Pablo Escobar",
+        imagen: "assets/animales/escobar.png",
+        audio: "assets/sonidos/escobar.mp3",
+        duracion: 2,
+        reproduciendo: false
+      },
+      {
+        nombre: "Dominic Toretto",
+        imagen: "assets/animales/toretto.png",
+        audio: "assets/sonidos/toretto.mp3",
+        duracion: 4,
+        reproduciendo: false
+      },
+      {
+        nombre: "Burro",
+        imagen: "assets/animales/burro.png",
+        audio: "assets/sonidos/burro.mp3",
+        duracion: 8,
+        reproduciendo: false
+      },
+      {
+        nombre: "John Cena",
+        imagen: "assets/animales/cena.png",
+        audio: "assets/sonidos/cena.mp3",
+        duracion: 5,
+        reproduciendo: false
+      },
+      {
+        nombre: "E.T",
+        imagen: "assets/animales/et.png",
+        audio: "assets/sonidos/et.mp3",
+        duracion: 2,
+        reproduciendo: false
+      },
+      {
+        nombre: "Cyborg",
+        imagen: "assets/animales/cyborg.png",
+        audio: "assets/sonidos/cyborg.mp3",
+        duracion: 2,
+        reproduciendo: false
+      },
+      {
+        nombre: "Han Solo",
+        imagen: "assets/animales/hansolo.png",
+        audio: "assets/sonidos/hansolo.mp3",
+        duracion: 2,
+        reproduciendo: false
+      },
+      {
+        nombre: "Scooby Doo",
+        imagen: "assets/animales/scooby.png",
+        audio: "assets/sonidos/sooby.mp3",
+        duracion: 2,
+        reproduciendo: false
+      }
+];
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ANIMALES } from '../../data/data.animales';
+import { Animal } from '../../interfaces/animal.interface';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,50 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  animales:Animal[] = [];
+  audio = new Audio();
+  tiempoaudio: any;
 
+
+  constructor(public navCtrl: NavController) {
+      this.animales = ANIMALES.splice(0);
+
+  }
+  private pausartodos(animalSel:Animal)
+  {
+      clearTimeout(this.tiempoaudio);
+      this.audio.pause();
+      this.audio.currentTime = 0;
+
+      for( let animal of this.animales)
+      {
+        if(animal.nombre != animalSel.nombre)
+        {
+          animal.reproduciendo = false;
+        }
+      }
+  }
+
+  reproducir(animal:Animal)
+  {
+    console.log(animal);
+
+    this.pausartodos(animal);
+
+    if(animal.reproduciendo)
+    {
+      animal.reproduciendo = false;
+      return;
+    }
+
+    //let audio = new Audio();
+    this.audio.src = animal.audio;
+    this.audio.load();
+    this.audio.play();
+
+    animal.reproduciendo = true;
+
+    this.tiempoaudio = setTimeout( () => animal.reproduciendo = false, animal.duracion*1000 );
   }
 
 }
